04b174c2f020fc46b36218a51794345497855460
leon <leon.tan@mail.utoronto.ca>
2019-10-07 19:29:00 -0400

    O(n)?

--- Main/Motif.java
+++ Main/Motif.java
@@ -2,12 +2,42 @@ class Motif {
 
     String motif(String miRNA, String dotForm) {
 
-        String mot = "";
-        for (int i = 0; i < miRNA.length(); i++) {
-            if (dotForm.charAt(i) == '.') {
-                mot += miRNA.charAt(i);
+        StringBuilder mot = new StringBuilder();
+        StringBuilder up = new StringBuilder();
+        StringBuilder down = new StringBuilder();
+        StringBuilder middle = new StringBuilder();
+        int num = findMiddle(dotForm);
+        for (int i = 0; i<num; i++) {
+            if (dotForm.charAt(i) == '.'){
+                up.append(miRNA.charAt(i));
+            }
+            if (dotForm.charAt(i) == '.' && dotForm.charAt(i + 1) == '('){
+                up.append(miRNA.charAt(i));
+                mot.append(up);
+                up.setLength(0);
+            }
+            if ()
+
+        }
+        for(int j = miRNA.length() -  1; j > num; j--){
+            if (dotForm.charAt(j) == '.'){
+                down.append(miRNA.charAt(j));
+            }
+            if (dotForm.charAt(j) == '.' && dotForm.charAt(j - 1) == ')'){
+                down.append(miRNA.charAt(j));
+                mot.append(down);
+                down.setLength(0);
             }
         }
-        return mot;
+        return mot.toString();
+    }
+
+    private int findMiddle(String dotForm){
+        for (int i = 0; i < dotForm.length(); i++) {
+            if (dotForm.charAt(i) == ')') {
+                return i;
+            }
+        }
+        return 0;
     }
 }
