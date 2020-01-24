d07fbff25c1b83459205609421145dfaadb8ca98
jackxu <sijie.xu@easygroup.ca>
2020-01-23 23:14:43 -0500

    Updating

--- Fa_Dot Convertion/RNA_Main.sh
+++ Fa_Dot Convertion/RNA_Main.sh
@@ -8,7 +8,7 @@ echo Please Enter the filename:
 
 read -p 'Filename: ' fn
 
-awk '/>/{close(x);x="S"++i".fa";}{print > x;}' fn.fa
+awk '/>/{close(x);x="S"++i".fa";}{print > x;}' ${fn}.fa
 
 for file in ./splited/*.fa
 do
@@ -16,9 +16,9 @@ do
     ./exe/ct2dot "${file%.fa}.ct" 1 "${file%.fa}.dot"
 done
 
-cat ./splited/*.dot >> $fn.dot
+cat ./splited/*.dot >> ${fn}.dot
 
-mv $fn.dot ../Main/$fn.dot
+mv $fn.dot ../Main/${fn}.dot
 
 rm ./splited/*.fa
 rm ./splited/*.ct
